9ba45e48d852d128bc6968b8832fee04629ff07a
ohadk <ohadkachlon@gmail.com>
2023-01-20 08:45:05 +0200

    1.1

---
+++ Ex1-1-yes-or-no.js
@@ -0,0 +1,4 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. 
+const bool = false;    //false or true
+const str = bool === false ? 'No' : 'Yes';
+console.log(str);
